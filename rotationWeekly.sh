a810a919a63fc64a785d07f1bcddb29bd692bb40
chuanmi <39377923+chuanmi@users.noreply.github.com>
2018-06-27 17:29:55 +0200

    Update rotationWeekly.sh

--- rotationWeekly.sh
+++ rotationWeekly.sh
@@ -6,22 +6,22 @@ date /t >>logrotation.txt
 time /t >>logrotation.txt
 
 ssh -i "/cygdrive/c/certificate" user@IP \
-							"mv /mnt/nfs/PRECON/pre-gal/C/Datos /mnt/nfs/PRECON/pre-gal/C/temporal \
-							&& mv /mnt/nfs/PRECON/pre-gal/C/Datos.bkp /mnt/nfs/PRECON/pre-gal/C/Datos \
-							&& mv /mnt/nfs/PRECON/pre-gal/C/Datos/temporal /mnt/nfs/PRECON/pre-gal/C/Datos.bkp" >>logrotacion.txt
+					"mv /mnt/nfs/PRECON/pre-gal/C/Datos /mnt/nfs/PRECON/pre-gal/C/temporal \
+					&& mv /mnt/nfs/PRECON/pre-gal/C/Datos.bkp /mnt/nfs/PRECON/pre-gal/C/Datos \
+					&& mv /mnt/nfs/PRECON/pre-gal/C/Datos/temporal /mnt/nfs/PRECON/pre-gal/C/Datos.bkp" >>logrotacion.txt
 							
 rsync.exe -avz -e "/cygdrive/c/cwRsync/ssh -i /cygdrive/c/cwRsync/id_dsa" \ 
-							--modify-window=20 \
-							--include "+ */" \
-							--exclude "- *.[Pp][Ss][Tt]" \
-							"/Source" "user@IP:/Destination" >>logrotacion.txt
+									--modify-window=20 \
+									--include "+ */" \
+									--exclude "- *.[Pp][Ss][Tt]" \
+									"/Source" "user@IP:/Destination" >>logrotacion.txt
 
 
 rsync.exe -avz -e "/cygdrive/c/cwRsync/ssh -i /cygdrive/c/cwRsync/id_dsa" \
-					--modify-window=20 \
-					--include "+ */" \
-					--exclude "- *.[Pp][Ss][Tt]" \
-					"/Source" "user@IP:/Destination"  >>logrotacion.txt
+									--modify-window=20 \
+									--include "+ */" \
+									--exclude "- *.[Pp][Ss][Tt]" \
+									"/Source" "user@IP:/Destination"  >>logrotacion.txt
 
 echo "Weekly Rotatin Finished" >>logrotation.txt
 date /t >>logrotation.txt
